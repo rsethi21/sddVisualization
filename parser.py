b3639813d66e25a6db374b618e680f8ffb6b0181
Rohan Sethi <rsethi1@luc.edu>
2023-07-31 00:05:17 -0500

    added comments and documentation to parser.py

--- parser.py
+++ parser.py
@@ -16,10 +16,10 @@ class SDDReport:
                               "cause", "damage", "breakspec", "sequence", \
                                 "lesiontime", "particletype", "particleenergy", \
                                     "particletranslation", "particledirection", "particletime"]
-    dimensionsHeaders = ["xcenter", "ycenter", "zcenter", "xmax", "ymax", "zmax", "xmin", "ymin", "zmin"]
-    chromosomeInfoHeaders = ["structure", "chromsomeNumber", "chromatidNumber", "arm"]
-    damageInfoHeaders = ["numBases", "singleNumber", "dsbPresent"]
-    causeHeaders = ["identifier", "direct", "indirect"]
+    dimensionsHeaders = ["xcenter", "ycenter", "zcenter", "xmax", "ymax", "zmax", "xmin", "ymin", "zmin"] # default column headers for sdd dimensions
+    chromosomeInfoHeaders = ["structure", "chromsomeNumber", "chromatidNumber", "arm"] # default column headers for chromosome information
+    damageInfoHeaders = ["numBases", "singleNumber", "dsbPresent"] # default column headers for damage information
+    causeHeaders = ["identifier", "direct", "indirect"] # default column headers for damage causes
     
     def __init__(self, sddPath: str):
         
@@ -33,16 +33,16 @@ class SDDReport:
         
         The goal of this function is to split the string and convert to desired output.  Class method since no need for instance specific changes.
         '''
-        sep = " / "
-        values = list(val.split(sep))
-        if type(values[0]) != typ:
-            if typ == type(0):
+        sep = " / " # common outer header for sdd file
+        values = list(val.split(sep)) # list of separated values
+        if type(values[0]) != typ: # below converting data into proper types
+            if typ == type(0): # converting to int
                 values = [int(value) for value in values]
-            elif typ == type(""):
+            elif typ == type(""): # converting to str
                 values = [str(value) for value in values]
-            elif typ == type(0.0):
+            elif typ == type(0.0): # converting to float
                 values = [float(value) for value in values]
-            else:
+            else: # unknown data type skip
                 output = f"Unknown type for entry {val}. Defaulted to existing type: {type(val)}"
                 print(output)
                 print()
@@ -57,16 +57,16 @@ class SDDReport:
         
         The goal of this function is to split the string and convert to desired output. Class method since no need for instance specific changes.
         '''
-        sep = ", "
-        values = list(val.split(sep))
-        if type(values[0]) != typ:
-            if typ == type(0):
+        sep = ", " # common inner header for sdd file
+        values = list(val.split(sep)) # list of separated values
+        if type(values[0]) != typ: # below converting data into proper types
+            if typ == type(0): # converting to int
                 values = [int(value) for value in values]
-            elif typ == type(""):
+            elif typ == type(""): # converting to str
                 values = [str(value) for value in values]
-            elif typ == type(0.0):
+            elif typ == type(0.0): # converting to float
                 values = [float(value) for value in values]
-            else:
+            else: # unknown data type skip
                 output = f"Unknown type for entry {val}. Defaulted to existing type: {type(val)}"
                 print(output)
                 print()
@@ -79,94 +79,115 @@ class SDDReport:
         inputs: requires a string that has slashes and then commas as a delimiter, and final type of the split values
         outputs: return a list of values after delimitation
         
-        The goal of this function is split values that are delimited by slashes then commas which is common in an sdd file.
+        The goal of this function is split values that are delimited by slashes then commas which is common in an sdd file. Class method since no need for instance specific changes.
         '''
-        lst = SDDReport.splitSlashes(val, type(""))
+        lst = SDDReport.splitSlashes(val, type("")) # split outer first
         vals = []
         for v in lst:
-            vals.append(SDDReport.splitCommas(v, typ))
+            vals.append(SDDReport.splitCommas(v, typ)) # split inner second
         return vals
 
     @classmethod
-    def openNStore(cls, path):
+    def openNStore(cls, path: str):
+        '''
+        inputs: path for sdd
+        outputs: opened DF
         
-        skiprow = 0
-        columnrow = list()
+        The goal of this function is to open an SDD and separate into its individuals columns unparsed and without header. Class method since no need for instance specific changes.
+        '''
+        skiprow = 0 # determining row at which the actual data starts
+        columnrow = list() # boolean list for which columns are present in the SDD file
 
-        file = open(path, "r")
-        lines = file.readlines()
+        file = open(path, "r") # opening file
+        lines = file.readlines() # reading file lines as list
 
-        for i, line in enumerate(lines):
-            if "EndOfHeader" in line:
-                skiprow = i+1
-            if "Data entries" in line:
-                columnrow = line[line.index("Data entries")+len("Data entries")+2:-2].split(", ")
-                columnrow = [True if item == "1" else False for item in columnrow]
+        for i, line in enumerate(lines): # iterating through list
+            if "EndOfHeader" in line: # looking for end of header marker to determine where data rows begin
+                skiprow = i+1 # assigning the index were data starts
+            if "Data entries" in line: # looking for binary data list to determine which columns present
+                columnrow = line[line.index("Data entries")+len("Data entries")+2:-2].split(", ") # finding string list with binary info. and spliting into list of ints
+                columnrow = [True if item == "1" else False for item in columnrow] # converting 1, 0s to booleans
 
-        with open(path, "r") as file2:
-            df = pd.read_csv(file2, sep=";", header = None, skiprows = skiprow)
-            df.dropna(axis=1, how="all", inplace=True)
+        with open(path, "r") as file2: # opening file again to read sdd
+            df = pd.read_csv(file2, sep=";", header = None, skiprows = skiprow) # opening sdd as a DF with appropriate skipping
+            df.dropna(axis=1, how="all", inplace=True) # remove columns with all NAs if any present (separator is a bit odd)
 
-            columns = list(compress(cls.originalColumnHeaders, columnrow))
-            df.columns = columns
+            columns = list(compress(cls.originalColumnHeaders, columnrow)) # applying boolean list to default column headers
+            df.columns = columns # setting default column headers
         
         return df
     
-    def extractCol(self, colName):
-
+    def extractCol(self, colName: str):
+        '''
+        inputs: colName as assigned in DF
+        outputs: pandas series object with specified column
+        
+        The goal of this function is basic function to get column info. Instance method because specific parsed SDD
+        '''
         return self.originalDF[colName]
 
     def parseVizInfo(self):
-
+        '''
+        inputs: none
+        outputs: dataframes of dimensions, chromosomeInfo, damageInfo, cause
+        
+        The goal of this function is is to extract plotting specific information for visualization.
+        '''
+        # each try and except below is in case the column does not exist
         try:
-            dimensions = []
-            for row in self.extractCol("xyz"):
-                temp = []
-                for l in SDDReport.splitBoth(row, type(0.0)):
-                    temp += l
-                dimensions.append(temp)
-            length = len(dimensions[0])
-            dimensions = pd.DataFrame(np.array(dimensions), columns=SDDReport.dimensionsHeaders[0:length])
+            dimensions = [] # instantiating dimensions list
+            for row in self.extractCol("xyz"): # iterating through rows of the coordinate columns
+                temp = [] # temporary list to store values from each row
+                for l in SDDReport.splitBoth(row, type(0.0)): # split values into floats
+                    temp += l # extend temp list with values from row
+                dimensions.append(temp) # append values from row
+            length = len(dimensions[0]) # determine the number of columns (center, max, min)
+            dimensions = pd.DataFrame(np.array(dimensions), columns=SDDReport.dimensionsHeaders[0:length]) # assign appropriate parsed column headers
         except ValueError:
             print("Either no positional information or missing extent of damage.")
 
         try:
-            chromosomeInfo = []
-            for row2 in self.extractCol("chromosomeid"):
-                chromosomeInfo.append(SDDReport.splitCommas(row2, type(0)))
-            chromosomeInfo = pd.DataFrame(np.array(chromosomeInfo), columns=SDDReport.chromosomeInfoHeaders)
+            chromosomeInfo = [] # instantiating chromosomeInfo list
+            for row2 in self.extractCol("chromosomeid"): # iterating through rows of the chromosomeID columns
+                chromosomeInfo.append(SDDReport.splitCommas(row2, type(0))) # split values into ints
+            chromosomeInfo = pd.DataFrame(np.array(chromosomeInfo), columns=SDDReport.chromosomeInfoHeaders) # assign appropriate parsed column headers
         except:
             print("There is no damage information column in this file. Skipping...")
             chromosomeInfo = None
 
         try:
-            damageInfo = []
-            for row3 in self.extractCol("damage"):
-                damageInfo.append(SDDReport.splitCommas(row3, type(0)))
-            damageInfo = pd.DataFrame(np.array(damageInfo), columns=SDDReport.damageInfoHeaders)
+            damageInfo = [] # instantiating damageInfo list
+            for row3 in self.extractCol("damage"): # iterating through rows of the damageInfo columns
+                damageInfo.append(SDDReport.splitCommas(row3, type(0))) # split values into ints
+            damageInfo = pd.DataFrame(np.array(damageInfo), columns=SDDReport.damageInfoHeaders) # assign appropriate parsed column headers
         except:
             print("There is no damage information column in this file. Skipping...")
             damage = None
 
         try:
-            cause = []
-            for row4 in self.extractCol("cause"):
-                cause.append(SDDReport.splitCommas(row4, type(0)))
-            cause = pd.DataFrame(np.array(cause), columns=SDDReport.causeHeaders)
+            cause = [] # instantiating cause list
+            for row4 in self.extractCol("cause"): # iterating through rows of then cause columns
+                cause.append(SDDReport.splitCommas(row4, type(0))) # split values into ints
+            cause = pd.DataFrame(np.array(cause), columns=SDDReport.causeHeaders) # assign appropriate parsed column headers
         except:
             print("There is no cause information column in this file. Skipping...")
             cause = None
 
         return dimensions, chromosomeInfo, damageInfo, cause
 
-    def saveParsed(self, df1, *dfs, path = None):
-
-        finaldf = df1
-        for df in dfs:
-            finaldf = finaldf.join(df)
+    def saveParsed(self, df1: pd.DataFrame, *dfs: pd.DataFrame, path: str = None):
+        '''
+        inputs: single or and number of dataframes, path to output path (optional)
+        outputs: final dataframe returned
         
-        self.parsedDf = finaldf
-        if path != None:
-            finaldf.to_csv(path)
+        The goal of this function is to combine the parsed columns needed for the final visualization.
+        '''
+        finaldf = df1 # set the single dataframe
+        for df in dfs: # iterating through each dataframe in the list
+            finaldf = finaldf.join(df) # join the dataframe columns into one big dataframe
+        
+        self.parsedDf = finaldf # set parsedDF as an value of the object
+        if path != None: # if path is None then does not save to a file otherwise saves to path
+            finaldf.to_csv(path) # saves to path
 
         return finaldf
