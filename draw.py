c5dc04c08440d43317f77a9995d115db026187cc
Rohan Sethi <rsethi1@luc.edu>
2023-08-16 22:34:43 -0500

    made coloring made consistent

--- draw.py
+++ draw.py
@@ -175,7 +175,6 @@ def graph(df: pd.DataFrame, labelCoordinateList: list, outputDir: str, volumes: 
   The goal of this function is to plot the graph with points/lines of damage and labelled/filtered as desired by the user. The png files will be labelled by filtration criteria and a basic one without labels
   '''
   colorlist = list(mcolors.CSS4_COLORS) # various matplotlib colors
-  random.shuffle(colorlist) # shuffling to get better color selections in the beginning of the list
 
   for key in labelCoordinateList: # iterate through list of labels
     print(f"Creating graph labelled by {key}...")
